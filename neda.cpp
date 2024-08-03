4350a35695f134f68c47be889c7027c9c6545105
EandT <mishravedant285@gmail.com>
2024-08-04 01:26:28 +0530

    new files

---
+++ neda.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s="Vedant Mishra ";
+    // for(int i=0;i<6; i++)
+    // {
+    //     cout<<arr[i];
+    // }
+    // cout<<arr[4];
+    cout<<s.length()<<endl;
+    cout<<s.size()<<endl;
+
+}
